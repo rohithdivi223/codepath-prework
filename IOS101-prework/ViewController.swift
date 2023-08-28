378a3517eeeb77445ef3bd824219b3c32d773fd4
rohithdivi223 <rdivi223@gmail.com>
2023-08-28 04:19:53 +0300

    Added prework files to repository

---
+++ IOS101-prework/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  IOS101-prework
+//
+//  Created by Rohith Divi on 8/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+        
+    }
+    
+}
+
